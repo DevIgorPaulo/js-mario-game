6726b52a8ad6bb96eda51feb7b88967fea952e4e
Igor Francisco Antunes Paulo <115815356+DevIgorPaulo@users.noreply.github.com>
2025-01-11 16:08:33 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,34 +1,50 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const mario = document.querySelector('.mario');
-    const pipe = document.querySelector('.pipe');
+    document.addEventListener('DOMContentLoaded', () => {
+        const mario   = document.querySelector('.mario');
+        const pipe    = document.querySelector('.pipe');
+        const counter = document.querySelector('#counter');
+    
+        let cont = 0;
+        let pulando = false;
+        let pontuar = false;
 
-    const jump = () => {
-        mario.classList.add('jump');
+        const jump = () => {
 
-        setTimeout(() =>{
-            mario.classList.remove('jump');
-        }, 500)
-    }
+            if(pulando) return;
 
-    const loop = setInterval(() => {
-        const pipePosition = pipe.offsetLeft;
-        const marioPostion = +window.getComputedStyle(mario).bottom.replace('px', '');
+            mario.classList.add('jump');
 
-        if(pipePosition <= 120 && pipePosition > 0 && marioPostion <= 80){
-            pipe.style.animation = 'none';
-            pipe.style.left = `${pipePosition}px`;
+            pulando = true;
+            pontuar = true;
 
-            mario.style.animation = 'none';
-            mario.style.bottom = `${marioPostion}px`;
-
-            mario.src = '../images/game-over.png'
-            mario.style.width = '75px'
-            mario.style.marginLeft = '50px'
-
-            clearInterval(loop)
+            setTimeout(() =>{
+                mario.classList.remove('jump');
+                pulando = false;
+            }, 500)
         }
 
-    })
+        const loop = setInterval(() => {
+            const pipePosition = pipe.offsetLeft;
+            const marioPostion = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    document.addEventListener('keydown', jump);
-});
+            if(pipePosition <= 120 && pipePosition > 0 && marioPostion <= 80){
+                pipe.style.animation = 'none';
+                pipe.style.left = `${pipePosition}px`;
+
+                mario.style.animation = 'none';
+                mario.style.bottom = `${marioPostion}px`;
+
+                mario.src = '../images/game-over.png'
+                mario.style.width = '75px'
+                mario.style.marginLeft = '50px'
+
+                clearInterval(loop)
+            } else if(pipePosition <= 120 && pipePosition > 0 && marioPostion > 80 && pontuar){
+                cont ++;
+                counter.innerHTML = "Points: " + cont;        
+                pontuar = false;
+            }
+
+        })
+
+        document.addEventListener('keydown', jump);
+    });
